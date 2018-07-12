89ae9eeaebff8fb16bfafcdd660e4fc518636702
Mrudula09 <mrudula.nudurupati1997@gmail.com>
2018-07-12 23:52:49 +0530

    changed database settings

--- stackoverflowClone/settings.py
+++ stackoverflowClone/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'stackoverflowClone.wsgi.application'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['aans1a9soaiykj'],
+            'USER': os.environ['admin'],
+            'PASSWORD': os.environ['pass1234'],
+            'HOST': os.environ['aans1a9soaiykj.ce0tcepjjtte.us-west-2.rds.amazonaws.com'],
+            'PORT': os.environ['3306'],
         }
     }
 else:
